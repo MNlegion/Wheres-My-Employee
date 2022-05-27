ffb4ffa3e764c9f251afa2d0e4a4c256f00ef054
MNlegion <rkreuser30@gmail.com>
2022-05-26 20:42:59 -0500

    connection and env setup

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,5 +5,5 @@ INSERT INTO roles(title, salary, department_id)
 VALUES("Engineer", 90000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("General Counsel", 300000, 5);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Bob', 'Smith', 1, 2), ('James', 'Smith', 1, null), ('Peyton', 'Manning', 1, 2), ('Jimmy', 'Jones', 2, 2), ('Larry', 'Legal', 5, null);
+VALUES ('Bob', 'Smith', 1, 2), ('James', 'Smith', 1, null), ('Peyton', 'Manning', 1, 2), ('Jimmy', 'Jones', 2, 2), ('Larry', 'Legal', 4, null);
 
